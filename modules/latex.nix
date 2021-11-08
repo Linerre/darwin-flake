6f08c066c665c713e44d9a4250ebaa2f24e7c4a4
Noel Errenil <leon.errelin@outlook.com>
2021-11-08 23:42:08 +0800

    Upate

--- modules/latex.nix
+++ modules/latex.nix
@@ -12,18 +12,11 @@
     extraPackages = tpkgs: {
       inherit (tpkgs)
         # collection-{basic,latex}
-        scheme-basic
+        collection-langenglish
         
         # a fun comment: https://github.com/mark-gerarts/m/blob/a9737c6935dd267f00d445f4e2fc5cff1ed1bedd/nix/texlive.nix#L9
         # https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/tools/typesetting/tex/texlive/pkgs.nix
         # other essentials
-        collection-luatex
-        collection-mathscience
-        collection-metapost
-        collection-xetex
-        collection-bibtexextra
-        collection-pictures
-        collection-bibtexextra
          
     };
   ;
