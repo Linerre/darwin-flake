f90cf957150252a12083171d105059382d6fd542
Linerre <leon.errelin@outlook.com>
2022-03-18 12:44:41 +0800

    Update Emacs config

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -2,11 +2,11 @@
 {
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/emacs-overlay/archive/097ce02d1c8d60afe55da8274b018fb7107584eb.tar.gz;
-      sha256 = "1y2xj5wsfan9hwqd2jw0aris0dylbc3jnkd153qbfnb3ybzv77v8";
+      url = https://github.com/nix-community/emacs-overlay/archive/5b6fc7a4ef21e8ca787429339476b82165b05f3d.tar.gz;
+      sha256 = "1fq71779vz6cmgfbziqwkpyzdc6zr3am3ls7x5l529rvbnzajgjk";
     }))
   ];
-  
+
   home-manager.users.errenil.programs.emacs = {
     enable = true;
     package = pkgs.emacsGcc;
