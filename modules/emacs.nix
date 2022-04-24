1caad0eaa359b688a5a08f8e44f0abd6bcfae20f
Linerre <leon.errelin@outlook.com>
2022-04-24 15:54:01 +0800

    Update Emacs build

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -1,14 +1,15 @@
 { pkgs, ... }:
 {
+
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/emacs-overlay/archive/5b6fc7a4ef21e8ca787429339476b82165b05f3d.tar.gz;
-      sha256 = "1fq71779vz6cmgfbziqwkpyzdc6zr3am3ls7x5l529rvbnzajgjk";
+      url = https://github.com/nix-community/emacs-overlay/archive/6216f7c7e05c5f7d579dc898219207a8509be910.tar.gz;
+      sha256 = "16i90wn14p89vx9zbfsm3yqy93n434fn02n5s1anr6gav4pnp56n";
     }))
   ];
 
   home-manager.users.errenil.programs.emacs = {
     enable = true;
-    package = pkgs.emacsGcc;
+    package = pkgs.emacsNativeComp;
   };
 }
